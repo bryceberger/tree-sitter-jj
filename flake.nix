97c03c522f62c54e3e675b231673cec1e9713316
Bryce Berger <bryce.z.berger@gmail.com>
2025-07-09 12:30:32 -0400

    init flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        tree-sitter
+        nodejs_24
+        typescript-language-server
+      ];
+    };
+  };
+}
